7d22059df2ee8cdce7115ca7b56c7c2063c3f134
Omar_Mohamed_1 <omar.abdel-aal00@eng-st.cu.edu.eg>
2021-10-09 14:32:30 +0200

    finished translation of landing page

--- App.js
+++ App.js
@@ -1,5 +1,6 @@
 import Landing from "./screens/Landing_Page";
 import React from "react";
+import i18n from './languages/i18n';
 export default function App() {
   return (
     <Landing/>
--- screens/Landing_Page.js
+++ screens/Landing_Page.js
@@ -1,14 +1,13 @@
-import React,{useState} from "react";
-import {Text,NativeBaseProvider,VStack,Box,Button, Link, HStack, ScrollView, View, Center} from "native-base";
+import React from "react";
+import {Text,NativeBaseProvider,VStack,Box,Button, Link, HStack, ScrollView, View, Center, Select} from "native-base";
 import { ImageBackground ,Dimensions,StyleSheet} from "react-native";
 import Therapy_session from '../assets/Img/Therapy_Session.jpeg';
-import { faSignInAlt,faUserPlus,faLanguage } from "@fortawesome/free-solid-svg-icons";
+import { faSignInAlt,faUserPlus,faLanguage, faGlobe } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-native-fontawesome";
 import Slidercarousel from "../components/Slider";
-
+import { useTranslation } from "react-i18next";
 
 const {width} = Dimensions.get("window");
-// const height =  width *0.6 ;
 const Quotes= [
   {
       body:"“ We cannot change anything until we accept it. Condemnation does not liberate, it oppresses.”",
@@ -26,31 +25,28 @@ const Quotes= [
     id:3
   },
 ];
+let globe = <FontAwesomeIcon   color="#dc2626" icon={faGlobe} />;
 export default function Landing() {
-  const [active,setActive]=useState(0);
-  const change=({nativeEvent})=>{
-    const slide = Math.ceil(nativeEvent.contentOffset.x / nativeEvent.layoutMeasurement.width);
-    if(slide !== active){
-      setActive(slide);
-    }
-  }
+  const {t,i18n} = useTranslation();
   return (
     <NativeBaseProvider>
         <VStack width="100%" height="100%">
-          
-          <Box height="60%" width="100%" borderBottomRightRadius={50}  bgColor="black"  >
+          <Box height="60%" width="100%" borderBottomRightRadius={50}  bgColor="black">
             <ImageBackground blurRadius={3}  borderBottomRightRadius={50} imageStyle={{opacity: 0.6}} source={Therapy_session} style={{flex:1}} resizeMode="cover" >
               <HStack>
-                <Text color="#FEFDFF" w="80%" fontSize="4xl" fontWeight="bold" mt="15%" mb="5%" ml="5%">Psy-Awareness</Text>
-                <Button w="10%" h="20%"  mt="20%" variant="subtle" bgColor="amber.600" _pressed={{bg:"gray.700"}} ><FontAwesomeIcon color="white" icon={faLanguage}/></Button>
+                <Text  color="#FEFDFF"  w="80%" fontSize="4xl" fontWeight="bold" mt="15%" mb="5%" ml="5%">Psy-Awareness</Text>
+                <Select mt="18%" dropdownIcon={globe} variant='unstyled'>
+                  <Select.Item label="Arabic" value="ar"  onPress={()=> i18n.changeLanguage("ar")} />
+                  <Select.Item label="English" value="en"  onPress={()=> i18n.changeLanguage("en")}/>
+                </Select>
               </HStack>
               <Text color="#FEFDFF" w="90%" fontSize="xl"  ml="5%" fontWeight='bold'>
-                  A Personal Psychiatrist at the Palm of your hand
+                  {t('A Personal Psychiatrist at the Palm of your hand')}
               </Text>
               <VStack alignItems="center" mt="12%">
-                <Button  mb={7} size="md" borderRadius={25} px={20}><Text fontSize="xl" color="#FEFDFF" fontWeight="bold">Sign-Up <FontAwesomeIcon color="#FEFDFF" icon={ faSignInAlt } /> </Text></Button>
-                <Button borderRadius={25} px={20} variant="outline"><Text fontSize="xl" color="#FEFDFF" fontWeight="bold">Sign-In <FontAwesomeIcon color="#FEFDFF" icon={ faUserPlus } /></Text></Button>
-                <Link><Text color="amber.600" fontWeight="bold" style={{textDecorationLine:"underline"}}  mt={5}>Are you a doctor?</Text></Link>
+                <Button  mb={7} size="md" borderRadius={25} px={20}><Text fontSize="lg" color="#FEFDFF" fontWeight="bold">{t('Sign-Up')} <FontAwesomeIcon color="#FEFDFF" icon={ faSignInAlt } /> </Text></Button>
+                <Button borderRadius={25} px={20} variant="outline"><Text fontSize="lg" color="#FEFDFF" fontWeight="bold">{t('Sign-In')} <FontAwesomeIcon color="#FEFDFF" icon={ faUserPlus } /></Text></Button>
+                <Button variant="link"><Text color="amber.600" fontWeight="bold" style={{textDecorationLine:"underline"}}  mt={5}>{t('Are you a doctor?')}</Text></Button>
               </VStack>
             </ImageBackground>
           </Box>
