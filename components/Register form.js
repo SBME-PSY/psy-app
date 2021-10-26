35924c7a485b18ec9aa56b584bb9618b86eb45ca
Omar_Mohamed_1 <omar.abdel-aal00@eng-st.cu.edu.eg>
2021-10-26 13:33:55 +0200

    added form validation (password not  validated)

--- components/Register form.js
+++ components/Register form.js
@@ -1,31 +1,45 @@
-import { NativeBaseProvider,VStack,Center,Box,Text,Heading, FormControl,Input, Icon,  Select, Button, ScrollView} from "native-base";
+import { NativeBaseProvider,VStack,Center,Box,Text,Heading, FormControl,Input, Icon,  Select, Button, ScrollView, Spinner} from "native-base";
 import React, { useState } from "react";
 import { useTranslation } from "react-i18next";
 import { FontAwesomeIcon } from "@fortawesome/react-native-fontawesome";
 import {  faCertificate, faEnvelope, faEye, faHome, faLock, faPhone, faUser } from "@fortawesome/free-solid-svg-icons";
 import Imageupload from "./Image Upload";
 import { Formik } from "formik";
+import * as yup from 'yup';
+import { alignContent, flex } from "styled-system";
+
+
 
 const Registerform = ({name,address,isdoctor,certificate,certificate_pic_label,signup}) =>{
-    const {t,i18n} = useTranslation();
     const [isPasswordShown,setIsPasswordShown]=useState(false);
+    const {t,i18n} = useTranslation();
+    const ReviewSchema= yup.object({
+        Username: yup.string().required(t('Your name is Required')).min(5,t('minimum letters in the name field is 5')),
+        email: yup.string().required(t('Email is Required')).email(t('your Email format is not right')),
+        // password: yup.string().matches(/^.*(?=.{8,})((?=.*[!@#$%^&*()\-_=+{};:,<.>]){1})(?=.*\d)((?=.*[a-z]){1})((?=.*[A-Z]){1}).*$/,t("Password must contain at least 8 characters, one uppercase, one number and one special case character")).required(t('Password is Required')),
+    })
     return(
         <NativeBaseProvider>
                 <Box width="100%">
                     <VStack>
                         <Center mt="10%" px={5}>
-                            <Formik initialValues={{Username:'',email:'',certificate:'',password:'',sex:'',marital_status:'',address:'',phone_num:''}}
-                                onSubmit={(values)=>{ console.log(values)}}>
+                            <Formik initialValues={{Username:'',email:'',password:'',certificate:'',sex:'',marital_status:'',address:'',phone_num:''}}
+                                onSubmit={(values)=> console.log(values) }
+                                validationSchema={ReviewSchema}
+                                >
                                 {(props)=>(
                                     <>
-                                    <FormControl isRequired >
+                                    <FormControl isRequired>
                                         <FormControl.Label _text={{color:'#003049'}}>{t(name)}</FormControl.Label>
                                             <Input
                                                 variant="underlined"
                                                 placeholder= {t('Username')}
                                                 onChangeText={props.handleChange('Username')}
                                                 value={props.values.Username}
-                                                InputLeftElement={<Icon as={<FontAwesomeIcon  icon={faUser} />}  mr={5} />}/>
+                                                InputLeftElement={<Icon as={<FontAwesomeIcon  icon={faUser} />}  mr={5} />}
+                                                onBlur={props.handleBlur('Username')}
+                                                />
+                                        <Text color='danger.500' >{ props.touched.Username &&  props.errors.Username}</Text>
                                     </FormControl>
                                     <FormControl my={2} isRequired>
                                         <FormControl.Label _text={{color:'#003049'}}>{t('Profile Picture')}</FormControl.Label>
@@ -35,23 +49,27 @@ const Registerform = ({name,address,isdoctor,certificate,certificate_pic_label,s
                                         <FormControl.Label _text={{color:'#003049'}}>{t('Email')}</FormControl.Label>
                                         <Input 
                                             onChangeText={props.handleChange('email')}
+                                            onBlur={props.handleBlur('email')}
                                             value={props.values.email}
                                             variant="underlined"
                                             placeholder= {t('Email')}
-                                            InputLeftElement={<Icon as={<FontAwesomeIcon  icon={faEnvelope} />}  mr={5} />}/>
+                                            InputLeftElement={<Icon as={<FontAwesomeIcon  icon={faEnvelope} />}  mr={5} />}
+                                            />
+                                            <Text color='danger.500' >{ props.touched.email && props.errors.email}</Text>
                                     </FormControl>
                                     <FormControl my={2} isRequired>
                                         <FormControl.Label _text={{color:'#003049'}}>{t('Password')}</FormControl.Label>
-                                        <FormControl.HelperText>{t("the password should be 8 characters. you can also use numbers and symbols")}</FormControl.HelperText>
+                                        <FormControl.HelperText>{t("the password should be 8 characters. you must also use 1 numbers and 1 special character like !")}</FormControl.HelperText>
                                         <Input  
                                             onChange={props.handleChange('password')}
+                                            onBlur={props.handleBlur('password')}
                                             value={props.values.password}
-                                            maxLength={8}
                                             type={isPasswordShown ? "text":"password"}
                                             variant="underlined"
                                             InputRightElement={<Icon as={<FontAwesomeIcon  icon={faEye} />} onPress={()=> setIsPasswordShown(!isPasswordShown)}  />}
                                             placeholder= {t('Password')}
                                             InputLeftElement={<Icon as={<FontAwesomeIcon  icon={faLock} />}  mr={5} />}/>
+                                            <Text color='danger.500' >{ props.touched.password && props.errors.password}</Text>
                                     </FormControl>
                                     {isdoctor && (
                                         <>
@@ -73,15 +91,15 @@ const Registerform = ({name,address,isdoctor,certificate,certificate_pic_label,s
                                     <FormControl my={2} isRequired>
                                         <FormControl.Label><Text color='#003049'>{t('choose your gender')}</Text></FormControl.Label>
                                         <Select onValueChange={props.handleChange('sex')} value={props.values.sex} placeholder={t('Sex')}>
-                                            <Select.Item label={t('Male')} value="Male"></Select.Item>
-                                            <Select.Item label={t('Female')} value="Female"></Select.Item>
+                                            <Select.Item label={t('Male')} value={t('Male')}></Select.Item>
+                                            <Select.Item label={t('Female')} value={t('Male')}></Select.Item>
                                         </Select>
                                     </FormControl>
-                                    <FormControl my={2} isRequired>
+                                    <FormControl my={2} isRequired >
                                         <FormControl.Label><Text color='#003049'>{t('Marital Status')}</Text></FormControl.Label>
                                             <Select onValueChange={props.handleChange('marital_status')} value={props.values.marital_status} placeholder={t('Marital Status')}>
-                                                <Select.Item label={t('Married')} value="Married"></Select.Item>
-                                                <Select.Item label={t('Single')} value="Single"></Select.Item>
+                                                <Select.Item label={t('Married')} value={t('Married')}></Select.Item>
+                                                <Select.Item label={t('Single')} value={t('Single')}></Select.Item>
                                             </Select>
                                         </FormControl>
                                     <FormControl my={2} isRequired>
