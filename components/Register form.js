9ba07b7cf23f3c295ba9c527177ef308eafd1b93
OmarMohamed156 <om658036@gmail.com>
2021-12-15 13:08:12 +0200

    User and Doctor age field done

--- components/Register form.js
+++ components/Register form.js
@@ -24,6 +24,7 @@ const Registerform = ({navigation,Name_label,address_label,isdoctor,signup,role}
     const [isConfirmPasswordShown,setIsConfirmPasswordShown]=useState(false);
     const {t,i18n} = useTranslation();
     const ReviewSchema= yup.object().shape({
+        age: yup.string().required(t('Age is required')),
         phone: yup.string().required(t('Phone number is required')),
         name: yup.string().required(t('Your name is Required')).min(5,t('minimum letters in the name field is 5')),
         email: yup.string().required(t('Email is Required')).email(t('your Email format is not right')),
@@ -64,7 +65,7 @@ const Registerform = ({navigation,Name_label,address_label,isdoctor,signup,role}
                     <VStack>
                         <Center mt="10%" px={5}>
                             <Formik
-                            initialValues={{name:'',email:'',password:'',relativePhone:'',confirmPassword:'',sex:'',maritalStatus:'',address:'',phone:''}}
+                            initialValues={{name:'',email:'',password:'',age:'',relativePhone:'',confirmPassword:'',sex:'',maritalStatus:'',address:'',phone:''}}
                                 onSubmit={ (data,actions)=> {
                                     data["role"]= role;
                                     console.log(data)
@@ -117,6 +118,17 @@ const Registerform = ({navigation,Name_label,address_label,isdoctor,signup,role}
                                             InputLeftElement={<Icon as={<FontAwesomeIcon  icon={faEnvelope} />}  mr={5} />}
                                             />
                                             <Text color='danger.500' >{ props.touched.email && props.errors.email}</Text>
+                                    </FormControl>
+                                    <FormControl my={2} isRequired>
+                                        <FormControl.Label _text={{color:'#003049'}}>{t('Age')}</FormControl.Label>
+                                        <Input 
+                                            onChangeText={props.handleChange('age')}
+                                            value={props.values.age}
+                                            variant="underlined"
+                                            placeholder= {t('Age')}
+                                            keyboardType='phone-pad'
+                                            />
+                                            <Text color='danger.500' >{ props.touched.age && props.errors.age}</Text>
                                     </FormControl>
                                     <FormControl my={2} isRequired>
                                         <FormControl.Label _text={{color:'#003049'}}>{t('Password')}</FormControl.Label>
