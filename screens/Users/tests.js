7cff9cf360bfa9c4bb62493c7f9589a275cab112
OmarMohamed156 <om658036@gmail.com>
2022-07-14 03:11:11 +0200

    adjusted the scroll view behaviour

--- screens/Users/tests.js
+++ screens/Users/tests.js
@@ -1,5 +1,5 @@
 import React, { useState ,useEffect} from "react";
-import { NativeBaseProvider,VStack,Text,Card,Avatar, HStack, Center,Radio, Button,Checkbox, FormControl, ScrollView, Spinner} from "native-base";
+import { NativeBaseProvider,VStack,Text,Card,Avatar, HStack, Center,Radio, Button,Checkbox, FormControl, ScrollView, Spinner,Progress} from "native-base";
 import { useTranslation } from "react-i18next";
 import axios from "axios";
 import { I18nManager ,StyleSheet, ToastAndroid,Dimensions, Alert} from "react-native";
@@ -11,15 +11,16 @@ export default function tests({navigation,route}){
     const [responses,setResponses] = useState([])
     const [error,setError] = useState(false);
     const [loading,isLoading] = useState(true)
-    const [answered,setAnswered] = useState(false)
     let qCounter = 0
+    const [answeredQuestions,setAnsweredQuestions] = useState(qCounter)
     const testID = route.params._id
-    // let results={}
     const [results,setResults] = useState({})
+    
 
     const getQuestionaire = ()=>{
         axios.get(`/psy/questionnaires/${testID}`).then(res=>{
-            setResponses(res.data)
+            setResponses(res.data.data)
+            // setResultLength(responses.questions.length)
             isLoading(false)
         }).catch(error=>{
             console.log(error)
@@ -50,14 +51,15 @@ export default function tests({navigation,route}){
             results['questions'] = questions
         }
         qCounter++
+        setAnsweredQuestions(qCounter)
     }
 
     const handleSubmit = async () => {
-        results.questionnaireID = responses.data._id
-        results.category = responses.data.category
+        results.questionnaireID = responses._id
+        results.category = responses.category
         results.rules = {}
 
-        if(!results.questions || results.questions.length != qCounter ){
+        if(!results.questions || results.questions.length != qCounter){
             setError(true)
             setInterval(() => {
                 setError(false)
@@ -94,39 +96,45 @@ export default function tests({navigation,route}){
         <NativeBaseProvider>
             {loading && <HStack mt='10%' justifyContent='center' alignItems='center' ><Spinner size='lg'   color='success.300' /></HStack> }
             {!loading && 
-                <ScrollView>
                     <VStack safeArea >
                         <Center>
-                        <Text textAlign='center' my={2} px={2} color='warning.800'>{t('Desclaimer !!!, This test is only an indication you should visit a doctor to get a more trusted diagnosis')}</Text>
-                        {responses.data.questions.map((question,Index)=>{
+                        <Text textAlign='center'  fontWeight='bold' fontSize='md' my={2} px={2} color='warning.800'>{t('Desclaimer !!!, This test is only an indication you should visit a doctor to get a more trusted diagnosis')}</Text>
+                        <ScrollView py={2}  horizontal>
+                        {responses.questions.map((question,Index)=>{
                             return(
-                                <Center key={Index}>
-                                    <Text textAlign='center' fontSize='lg' fontWeight='bold' mt={2} key={Index} >{Index+1}  :  {I18nManager.isRTL ?   question.body.ar + " ؟" :question.body.en + " ?"} </Text>
+                            <Card mb={2} mx={1}  key={Index}>
+                                <Center width={Dimensions.get('window').width-40} >
+                                    <Text textAlign='center' pb={2} borderBottomWidth={2} borderBottomColor="black" fontSize='lg' fontWeight='bold' mt={2} key={Index} >{Index+1}  :  {I18nManager.isRTL ?   question.body.ar + " ؟" :question.body.en + " ?"} </Text>
                                     <Center>
                                         {question.answers.map((answer,index)=>{
                                             return(
-                                                <FormControl key={index} isRequired>
-                                                    <Radio.Group shadow={answered}  my={2} key={index} colorScheme="success" onChange={(val)=>{catchValue(val,responses.data.questions)}}>
-                                                        <Radio mx={5} value={I18nManager.isRTL ? answer.body.ar :answer.body.en}>
-                                                            <Text mx={1} >{I18nManager.isRTL ? answer.body.ar :answer.body.en}</Text>
-                                                        </Radio>
-                                                    </Radio.Group>   
-                                                </FormControl>
+                                                    <FormControl key={index} isRequired>
+                                                        <Radio.Group my={2} key={index} colorScheme="success" onChange={(val)=>{catchValue(val,responses.questions)}}>
+                                                            <HStack my={1} >
+                                                                <Radio mx={5} value={I18nManager.isRTL ? answer.body.ar :answer.body.en}>
+                                                                    <Text mx={1} >{I18nManager.isRTL ? answer.body.ar :answer.body.en}</Text>
+                                                                </Radio>
+                                                            </HStack>
+                                                        </Radio.Group>   
+                                                    </FormControl>
                                             )
                                         })}
                                     </Center>
                                 </Center>
+                            </Card>
                             )
                         })}
+                </ScrollView>
                         <FormControl>
+                            {/* <Progress mx={3} colorScheme="success"  min='0' max={responses.questions.length}  value={answeredQuestions} /> */}
+                            <Text>{t('Number of Questions Answered:')+'  '+ answeredQuestions}</Text>
                             <Center>
                                 {error && <Text italic={true} color='error.700'>{t('Please Answer all the above Questions')}</Text>}
-                                <Button width={200}  my={2} colorScheme='emerald' borderRadius={35} onPress={handleSubmit} >{t('Submit')}</Button>
+                                <Button width={200}  mt={12} colorScheme='emerald' borderRadius={35} onPress={handleSubmit} >{t('Submit')}</Button>
                             </Center>
                         </FormControl>
                         </Center>
                     </VStack>
-                </ScrollView>
             }
         </NativeBaseProvider>
     )
