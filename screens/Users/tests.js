bb264868be1e68df9359b4b22be80f9f2612e70a
OmarMohamed156 <om658036@gmail.com>
2022-07-15 21:42:16 +0200

    removed unused package

--- screens/Users/tests.js
+++ screens/Users/tests.js
@@ -5,7 +5,6 @@ import axios from "axios";
 import { I18nManager ,StyleSheet,Dimensions, Alert} from "react-native";
 import { TouchableOpacity } from "react-native";
 import getAuthData from "../../hooks/getAuthData"
-import { BaseToast } from "react-native-toast-message";
 
 
 export default function tests({navigation,route}){
